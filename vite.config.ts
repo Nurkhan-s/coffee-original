1788e5e60c5a52a32d49283c31f8bac63fadfb4f
Nurkhan <n.shakuali@amarkets.com>
2024-01-23 19:19:53 +0600

    feat: removed docs folder

--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,10 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  base: "/coffee-original/",
   plugins: [
     vue(),
   ],
-  build: {
-    outDir: 'docs'
-  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))

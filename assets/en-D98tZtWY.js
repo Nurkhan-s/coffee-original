6c87314ec02391c07c78ee82f4ac2c9ff71bd6fd
Nurkhan <n.shakuali@amarkets.com>
2024-01-27 17:08:03 +0600

    public content in orphan branch public

---
+++ assets/en-D98tZtWY.js
@@ -0,0 +1,1 @@
+const n={kaz:"Kazakh",ru:"Russian",en:"English"},a={config:"Configuration"},t={languages:n,title:a};export{t as default,n as languages,a as title};
---
+++ assets/ru-POsBdQ0d.js
@@ -0,0 +1,1 @@
+const a={kaz:"Kazakh",ru:"Russian",en:"English"},n={config:"Configuration"},t={languages:a,title:n};export{t as default,a as languages,n as title};
